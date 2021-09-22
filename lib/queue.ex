04378fafdded82988caaa1e18c5a83dfd43ae8e1
bruguedes <bruguedes@gmail.com>
2021-09-21 19:38:42 -0500

    chore: remove guard

--- lib/queue.ex
+++ lib/queue.ex
@@ -3,22 +3,22 @@ defmodule Queue do
 
   ############### ----GENSERVER----#############
   @impl true
-  def init(state) when is_list(state) do
+  def init(state) do
     {:ok, state}
   end
 
   @impl true
-  def handle_cast({:push, element}, state) do
+  def handle_cast({:enqueue, element}, state) do
     {:noreply, [element | state]}
   end
 
   @impl true
-  def handle_call(:pop, _from, [head | tail]) do
+  def handle_call(:dequeue, _from, [head | tail]) do
     {:reply, head, tail}
   end
 
   @impl true
-  def handle_call(:po, _from, []) do
+  def handle_call(:dequeue, _from, []) do
     {:reply, "Não exitem mais elementos na fila", []}
   end
 end
