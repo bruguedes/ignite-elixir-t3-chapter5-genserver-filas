23ddaa84de61805816f14a6c85775cb531f91293
bruguedes <bruguedes@gmail.com>
2021-09-21 19:20:23 -0500

    feat: wip

---
+++ lib/genserver_filas.ex
@@ -0,0 +1,18 @@
+defmodule GenserverFilas do
+  @moduledoc """
+  Documentation for `GenserverFilas`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenserverFilas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
